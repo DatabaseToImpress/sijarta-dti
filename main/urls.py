7923440875f0819818a026d993353c89e25d6167
syifa-ananda <syifaananda36@gmail.com>
2024-11-18 11:17:20 +0700

    test

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home_page, name='home_page'),
+    path('subcategory/<int:id>/', views.subcategory_services_user, name='subcategory_services_user'),
+    path('booking/', views.booking_view, name='booking'),
+    path('worker/<str:worker_name>/', views.worker_profile, name='worker_profile'),
+]
---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Example view for HomePage
+def home_page(request):
+    return render(request, 'home.html')
+
+def subcategory_services_user(request, id):
+    category = request.GET.get('category', None)
+    subcategory = request.GET.get('subcategory', None)
+    
+    context = {
+        'category': category,
+        'subcategory': subcategory,
+        'id': id,  # Add id to context so you can use it in the template
+    }
+    return render(request, 'subcategory_services_user.html', context)
+
+def booking_view(request):
+    return render(request, "booking.html")
+
+def worker_profile(request, worker_name):
+    context = {
+        'worker_name': worker_name,
+    }
+    return render(request, 'worker_profile.html', context)
